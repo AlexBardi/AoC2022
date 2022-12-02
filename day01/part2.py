6244fa332c36630d1c35e706d6779ae1b46d79b8
Alex Bardi <alex.bardi@hey.com>
2022-12-01 20:35:40 -0600

    Day 1 done!

--- day01/part2.py
+++ day01/part2.py
@@ -1,4 +1,5 @@
 import os
+import heapq
 
 dir_path = os.path.dirname(__file__)
 
@@ -6,17 +7,15 @@ input = open(dir_path + "\input.txt").read().strip('\n\n').split("\n")
 #input = open(dir_path + "\\test.txt").read().strip('\n\n').split("\n")
 
 currCount = 0
-maxCount = 0
+sums = []
 
 for line in input:
     if line == '':
-        if currCount > maxCount:
-            maxCount = currCount
+        heapq.heappush(sums, currCount)
         currCount = 0
     else:
         currCount += int(line)
 
-print(maxCount)
+print(sum(heapq.nlargest(3, sums)))
 
-# 672393 Too high <- I wasn't clearing currCount when it wasn't overwriting maxCount
-# 68775 *
+# 202585 *
