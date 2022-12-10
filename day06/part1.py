d73b96fbad732d219df8b8c0b799b796c714f970
Alex Bardi <alex.bardi@hey.com>
2022-12-10 10:17:19 -0500

    Day 6 part one was been solved

---
+++ day06/part1.py
@@ -0,0 +1,63 @@
+import os
+
+def incRange(endIdx, line, dict):
+    removedIdx = endIdx - 3
+    removedChar = line[removedIdx]
+    addedIdx = endIdx + 1
+    addedChar = line[addedIdx]
+    print(removedChar, addedChar)
+
+    # Sanity check, this should always be true
+    if removedChar in dict:
+        if dict[removedChar] > 1:
+            dict[removedChar] -= 1
+        else:
+            del dict[removedChar]
+    else:
+        print("Uhh, why did we try to remove a char that isn't in the dictionary?")
+        print(endIdx, line, dict)
+
+    if addedChar in dict:
+        dict[addedChar] += 1
+    else:
+        dict[addedChar] = 1
+
+    return
+
+dir_path = os.path.dirname(__file__)
+
+# input = open(dir_path + "\input.txt").read().strip('\n\n').split("\n")
+# input = open(dir_path + "\\test.txt").read().strip('\n\n').split("\n")
+
+input = open(dir_path + "/input.txt").read().strip('\n\n').split("\n")
+# input = open(dir_path + "/test.txt").read().strip('\n\n').split("\n")
+
+solns = []
+
+for line in input:
+    print(line)
+    #init first rectangle
+    dict = {}
+
+    for idx in range(4):
+        char = line[idx]
+        if char in dict:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+    endIdx = 3
+    if len(dict) == 4:
+        # solns aren't zero indexed
+        solns.append(endIdx + 1)
+        continue
+
+    while len(dict) < 4:
+        incRange(endIdx, line, dict)
+        endIdx += 1
+
+    solns.append(endIdx + 1)
+
+
+print(solns)
+# 1779 * 
+
